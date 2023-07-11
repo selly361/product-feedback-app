c495ca539a47f444a755ebd95f25fa5913c35d5e
seikou <seikou615@gmail.com>
2023-07-11 19:06:50 +0100

    Added: `userArray` constant

---
+++ src/constants/userArray.ts
@@ -0,0 +1,64 @@
+import { User } from "types"
+
+export const userArray: User[] = [
+    {
+      image: "./assets/user-images/image-zena.jpg",
+      name: "Zena Kelley",
+      username: "velvetround"
+    },
+    {
+      image: "./assets/user-images/image-suzanne.jpg",
+      name: "Suzanne Chang",
+      username: "upbeat1811"
+    },
+    {
+      image: "./assets/user-images/image-thomas.jpg",
+      name: "Thomas Hood",
+      username: "brawnybrave"
+    },
+    {
+      image: "./assets/user-images/image-elijah.jpg",
+      name: "Elijah Moss",
+      username: "hexagon.bestagon"
+    },
+    {
+      image: "./assets/user-images/image-james.jpg",
+      name: "James Skinner",
+      username: "hummingbird1"
+    },
+    {
+      image: "./assets/user-images/image-anne.jpg",
+      name: "Anne Valentine",
+      username: "annev1990"
+    },
+    {
+      image: "./assets/user-images/image-ryan.jpg",
+      name: "Ryan Welles",
+      username: "voyager.344"
+    },
+    {
+      image: "./assets/user-images/image-george.jpg",
+      name: "George Partridge",
+      username: "soccerviewer8"
+    },
+    {
+      image: "./assets/user-images/image-javier.jpg",
+      name: "Javier Pollard",
+      username: "warlikeduke"
+    },
+    {
+      image: "./assets/user-images/image-roxanne.jpg",
+      name: "Roxanne Travis",
+      username: "peppersprime32"
+    },
+    {
+      image: "./assets/user-images/image-victoria.jpg",
+      name: "Victoria Mejia",
+      username: "arlen_the_marlin"
+    },
+    {
+      image: "./assets/user-images/image-jackson.jpg",
+      name: "Jackson Barker",
+      username: "countryspirit"
+    }
+  ]
