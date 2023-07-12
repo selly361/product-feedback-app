3d170426a7332aa28b431d0aaefbd471e55307ae
seikou <seikou615@gmail.com>
2023-07-12 15:25:18 +0100

    Added more types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,34 +1,51 @@
+export type TStatus =
+  | "suggestion"
+  | "planned"
+  | "in-progress"
+  | "live"
+  | "bug"
+  | "feature"
+  | "enhancement";
+
+export type TCategory = "enhancement" | "feature" | "bug" | "ui" | "ux" | "all";
+
+export type TSortPreference =
+  | "Most Upvotes"
+  | "Least Upvotes"
+  | "Most Comments"
+  | "Least Comments";
+
 export interface User {
-  image: string
-  name: string
-  username: string
+  image: string;
+  name: string;
+  username: string;
 }
 
 interface Comment {
-  id: number
-  content: string
-  user: User
-  replies?: Reply[]
+  id: number;
+  content: string;
+  user: User;
+  replies?: Reply[];
 }
 
 interface Reply {
-  content: string
-  replyingTo: string
-  user: User
+  content: string;
+  replyingTo: string;
+  user: User;
 }
 
-interface ProductRequest {
-  id: number
-  title: string
-  category: string
-  upvotes: number
-  upvoted: string[]
-  status: string
-  description: string
-  comments?: Comment[]
+export interface ProductRequest {
+  id: number;
+  title: string;
+  category: string;
+  upvotes: number;
+  upvoted: string[];
+  status: string;
+  description: string;
+  comments?: Comment[];
 }
 
 export interface ProductFeedBack {
-  currentUser: User
-  productRequests: ProductRequest[]
+  currentUser: User;
+  productRequests: ProductRequest[];
 }
