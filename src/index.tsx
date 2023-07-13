1b3e22a706388e1f183194c87c0f0198308e6e99
seikou <seikou615@gmail.com>
2023-07-13 17:17:15 +0100

    Wrapped `App` with `BrowserRouter`

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,20 @@
-import App from './App';
-import { Provider } from 'react-redux';
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import reportWebVitals from './reportWebVitals';
-import { store } from './app/store';
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import React from "react";
+import { createRoot } from "react-dom/client";
+import reportWebVitals from "./reportWebVitals";
+import { store } from "./app/store";
 
-const container = document.getElementById('root')!;
+const container = document.getElementById("root")!;
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
