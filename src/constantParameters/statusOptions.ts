c7a2e83362160470f3b3cd20657ba09d922104b3
seikou <seikou615@gmail.com>
2023-07-13 17:16:16 +0100

    Added: `statusOptions` constant

---
+++ src/constantParameters/statusOptions.ts
@@ -0,0 +1,8 @@
+const statusOptions = [
+    { value: "suggestion", label: "Suggestion" },
+    { value: "planned", label: "Planned" },
+    { value: "in-progress", label: "In-progress" },
+    { value: "live", label: "Live" },
+  ];
+
+export { statusOptions }
