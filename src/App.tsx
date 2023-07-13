1dbd00c928160be0ae32af745e1272b6e83bafc1
seikou <seikou615@gmail.com>
2023-07-13 20:13:04 +0100

    Refactored `App` component, added routes, and included global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import { AddFeedBackPage, EditFeedBackPage, HomePage, ProductPage, RoadMapPage } from "pages";
+import { Route, Routes } from "react-router-dom";
 
-const App = () => {
+import { Fragment } from "react";
+import { GlobalStyle } from "global";
+
+function App() {
+
   return (
-    null
-  )
+    <Fragment>
+      <GlobalStyle />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/add" element={<AddFeedBackPage />} />
+        <Route path="/roadmap" element={<RoadMapPage />} />
+        <Route path="/edit/:id" element={<EditFeedBackPage />} />
+        <Route path="/:id" element={<ProductPage />} />
+      </Routes>
+    </Fragment>
+  );
 }
 
-export default App
+export default App;
