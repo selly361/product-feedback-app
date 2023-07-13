93752c90f38647e4e17a4da0096bff2cfdba1300
seikou <seikou615@gmail.com>
2023-07-13 17:16:38 +0100

    Exported `categoryOptions` and `statusOptions`

--- src/constantParameters/index.ts
+++ src/constantParameters/index.ts
@@ -1,2 +1,4 @@
 export { initialState } from "./initialState"
 export { userArray } from "./userArray"
+export { categoryOptions } from "./categoryOptions"
+export { statusOptions } from "./statusOptions"
