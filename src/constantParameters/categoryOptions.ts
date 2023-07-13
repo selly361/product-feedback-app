d323288baf4b6b2a295d813e3795fb5533629312
seikou <seikou615@gmail.com>
2023-07-13 17:16:00 +0100

    Added: `categoryOptions` constant

---
+++ src/constantParameters/categoryOptions.ts
@@ -0,0 +1,10 @@
+const categoryOptions = [
+    { value: "feature", label: "Feature" },
+    { value: "ui", label: "UI" },
+    { value: "ux", label: "UX" },
+    { value: "enhancement", label: "Enhancement" },
+    { value: "bug", label: "Bug" },
+  ];
+  
+
+export { categoryOptions }
