aac6ec90ad57d0095e91fe42fb2360b607542696
seikou <seikou615@gmail.com>
2023-07-14 22:07:36 +0100

    Added: `SideBar` components' styles

---
+++ src/components/PageTemplates/Home/SideBar/SideBar.styles.ts
@@ -0,0 +1,122 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+export const StyledAside = styled.aside`
+  display: grid;
+  gap: 24px;
+
+  section {
+    width: 255px;
+    min-height: 137px;
+    border-radius: 10px;
+  }
+`;
+
+export const TopSection = styled.section`
+  background-image: url("suggestions/desktop/background-header.png");
+  padding: 62px 72px 24px 24px;
+
+  * {
+    color: #fff;
+  }
+
+  h6 {
+    opacity: 0.75;
+  }
+`;
+
+export const MiddleSection = styled.section`
+  background: #fff;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 8px;
+
+  padding: 24px 0 24px 24px;
+`;
+
+export const BottomSection = styled.section`
+  background: #fff;
+  padding: 19px 24px 24px 24px;
+`;
+
+export const StyledButton = styled.button`
+  width: max-content;
+  height: 30px;
+
+  border-radius: 10px;
+  background: #f2f4ff;
+  color: #4661e6;
+  font-size: 13px;
+  font-weight: 600;
+
+  padding: 5px 16px 6px 16px;
+  display: grid;
+  place-items: center;
+
+  transition: 0.5s ease background, 0.5s ease color;
+
+  &.active {
+    color: #fff;
+    background: #4661e6;
+  }
+`;
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+
+  & > div:first-child {
+    margin-bottom: 16px;
+  }
+`;
+
+export const SubContainer = styled.div`
+  display: flex;
+  width: 100%;
+  height: max-content;
+  align-items: center;
+  justify-content: space-between;
+
+  div {
+    display: flex;
+    align-items: center;
+    gap: 16px;
+  }
+
+  h5 {
+    font-weight: 400;
+    color: #647196;
+
+  }
+`;
+
+export const StyledLink = styled(Link)`
+  color: #4661e6;
+  font-size: 13px;
+  font-weight: 600;
+  text-decoration-line: underline;
+
+  transition: .5s ease color;
+
+  &:hover {
+    color: #8397f8;
+  }
+`;
+
+export const Dot = styled.div<{ type: "planned" | "in-progress" | "live" }>`
+  width: 8px;
+  height: 8px;
+  border-radius: 4px;
+  
+  background-color: ${(props) =>
+    props.type === "planned"
+      ? "#F49F85"
+      : props.type === "in-progress"
+      ? "#AD1FEA"
+      : "#62BCFA"};
+`;
+
+export const Amount = styled.h5`
+    color: #647196;
+`
