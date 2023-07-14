aef7ff7ee09a72b515be8c31f5b051f83e98791e
seikou <seikou615@gmail.com>
2023-07-14 08:03:39 +0100

    Added: `buttonTypeVariants` constant

---
+++ src/constantParameters/buttonTypeVariants.ts
@@ -0,0 +1,18 @@
+export const buttonTypeVariants = {
+    1: {
+      backgroundColor: "#AD1FEA",
+      hoverBackgroundColor: "#C75AF6",
+    },
+    2: {
+      backgroundColor: "#4661E6",
+      hoverBackgroundColor: "#7C91F9",
+    },
+    3: {
+      backgroundColor: "#3A4374",
+      hoverBackgroundColor: "#656EA3",
+    },
+    4: {
+      backgroundColor: "#D73737",
+      hoverBackgroundColor: "#E98888",
+    },
+  };
