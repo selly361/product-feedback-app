97ad1caad5cde8316ed5192844de62b80e3e65ee
seikou <seikou615@gmail.com>
2023-07-15 22:11:22 +0100

    Updated styles

--- src/pages/Home/Home.styles.ts
+++ src/pages/Home/Home.styles.ts
@@ -1,1 +1,11 @@
 import { styled } from "styled-components";
+
+export const StyledHome = styled.main`
+    background-color: #F7F8FD;
+    width: 1110px;
+    
+    display: grid;
+    grid-template-columns: 255px 825px;
+    align-content: start;
+    gap: 30px;
+`
