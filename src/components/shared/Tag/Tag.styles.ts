307e10a5e3d5bcb25b47638fed34b8740160800f
seikou <seikou615@gmail.com>
2023-07-16 16:08:57 +0100

    Added: `Tag` Components' styles

---
+++ src/components/shared/Tag/Tag.styles.ts
@@ -0,0 +1,18 @@
+import { styled } from "styled-components";
+
+export const StyledTag = styled.div`
+  width: max-content;
+  height: 30px;
+
+  border-radius: 10px;
+  background: #f2f4ff;
+  color: #4661e6;
+  font-size: 13px;
+  font-weight: 600;
+
+  padding: 5px 16px 6px 16px;
+  display: grid;
+  place-items: center;
+
+  text-transform: capitalize;
+`;
