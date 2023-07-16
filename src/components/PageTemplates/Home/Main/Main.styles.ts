49db2f4c1adf3aac2e71d82b5bdcf8fd3a2f7406
seikou <seikou615@gmail.com>
2023-07-16 10:41:09 +0100

    Added: `Main` component styles

---
+++ src/components/PageTemplates/Home/Main/Main.styles.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components"
+
+export const StyledMain = styled.main`
+    width: 825px;
+    height: max-content;
+
+    display: grid;
+    gap: 24px;
+
+
+
+`
+
+
+export const Container = styled.div`
+    width: 825px;
+ 
+
+`
