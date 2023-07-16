37b528620f8e603681abd01b4ac10eac30189c1b
seikou <seikou615@gmail.com>
2023-07-16 12:28:24 +0100

    Refactored

--- src/components/PageTemplates/Home/Header/index.tsx
+++ src/components/PageTemplates/Home/Header/index.tsx
@@ -1,30 +1,26 @@
-import { ArrowDownIcon, NewFeedbackIcon, SuggestionsIcon } from "icons";
-import { Button, DropDown } from "components";
+import { ArrowDownIcon, NewFeedbackIcon, SuggestionsIcon } from "icons"
+import { Button, DropDown } from "components"
 import {
   Container,
   SortButton,
   SortButtonContainer,
   StyledHeader,
-} from "./Header.styles";
-import { useRef, useState } from "react";
+} from "./Header.styles"
+import { useRef, useState } from "react"
 
-import { useAppSelector } from "app/hooks";
-import { useClickOutside } from "hooks";
-import { useNavigate } from "react-router-dom";
+import { useAppSelector } from "app/hooks"
+import { useClickOutside } from "hooks"
+import { useNavigate } from "react-router-dom"
 
 function Header() {
-  const filteredSuggestions = useAppSelector(
-    (state) => state.filteredSuggestions
-  );
-  const sortPreference = useAppSelector(
-    (state) => state.filterConfig.sortPreference
-  );
-  const length = filteredSuggestions.length;
+  const filteredSuggestions = useAppSelector((state) => state.filteredSuggestions)
+  const sortPreference = useAppSelector((state) => state.filterConfig.sortPreference)
+  const length = filteredSuggestions.length
 
-  const [open, setOpen] = useState(false);
-  const sortButtonRef = useRef<HTMLButtonElement | null>(null);
+  const [open, setOpen] = useState(false)
+  const sortButtonRef = useRef<HTMLButtonElement | null>(null)
 
-  useClickOutside(sortButtonRef, () => setOpen(false));
+  useClickOutside(sortButtonRef, () => setOpen(false))
 
   const navigate = useNavigate()
 
@@ -50,7 +46,7 @@ function Header() {
       </Container>
       <Button content="+ Add Feedback" buttonType={1} onClick={() => navigate("/add")} />
     </StyledHeader>
-  );
+  )
 }
 
-export default Header;
+export default Header
