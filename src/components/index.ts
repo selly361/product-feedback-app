b358f906dd2fe76a4c26bcfbf52346ce3aa2de9e
seikou <seikou615@gmail.com>
2023-07-16 13:07:46 +0100

    Exported `Tag` component as default

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 export { default as Button } from "./shared/Button"
+export { default as Tag } from "./shared/Tag"
 export { default as SideBar } from "./PageTemplates/Home/SideBar"
 export { default as Header } from "./PageTemplates/Home/Header"
 export { default as Main } from "./PageTemplates/Home/Main"
