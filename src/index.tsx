921d07821ea900b5c82671aca4fa98219f54fa24
seikou <seikou615@gmail.com>
2023-07-17 16:03:54 +0100

    Wrapped `App` component with PersistGate for state persistence

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
+import { persistor, store } from "./app/store";
+
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from "react-redux";
 import React from "react";
 import { createRoot } from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
-import { store } from "./app/store";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
@@ -12,11 +14,15 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
+
+
 
 reportWebVitals();
