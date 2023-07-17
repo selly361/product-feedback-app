e200d7656e7908e657cd0d765b3089769e0c4731
seikou <seikou615@gmail.com>
2023-07-17 16:03:21 +0100

    Added: Redux Persist configuration to store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,22 @@
 import { Action, ThunkAction, configureStore } from '@reduxjs/toolkit';
+import { persistReducer, persistStore } from 'redux-persist';
 
 import productFeedbackReducer from '../features/product-feedback-app/productFeedbackSlice';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+  key: 'root', 
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, productFeedbackReducer);
 
 export const store = configureStore({
-  reducer: productFeedbackReducer
+  reducer: persistedReducer,
 });
+
+export const persistor = persistStore(store);
+
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
