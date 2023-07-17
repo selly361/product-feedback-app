510b57be175f960be92a45a46e31e28d18e64620
seikou <seikou615@gmail.com>
2023-07-17 17:36:44 +0100

    Added: `BackButton` components' styles

---
+++ src/components/shared/BackButton/BackButton.styles.ts
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+
+export const StyledBackButton = styled.button`
+  width: unset;
+  height: unset;
+  background-color: transparent;
+  display: flex;
+  align-items: center;
+  gap: 15px;
+  color: #647196;
+  font-size: 14px;
+  font-weight: 700;
+  position: relative;
+
+  &::before {
+    content: "";
+    position: absolute;
+    bottom: -2px;
+    left: 0;
+    right: 0;
+    margin: auto;
+    height: 1px;
+    width: 0;
+    transition: .5s ease width, .5s ease background;
+    background: #647196;
+    
+    
+  }
+
+  &:hover::before {
+    width: 100%;
+  }
+`;
+
+
