06838fca10611a6dfab7153385373b3e78fa8439
seikou <seikou615@gmail.com>
2023-07-17 15:54:00 +0100

    Added: `userProfile` component

---
+++ src/components/PageTemplates/Home/UserProfile/index.tsx
@@ -0,0 +1,32 @@
+import { StyledImage, StyledUserProfile, Username } from './UserProfile.styles'
+import { useAppDispatch, useAppSelector } from 'app/hooks'
+
+import React from 'react'
+import { changeUser } from 'features/product-feedback-app/productFeedbackSlice'
+import { userArray } from 'constantParameters'
+
+function UserProfile() {
+
+    const { username, image } = useAppSelector(state => state.currentUser)
+
+    const dispatch = useAppDispatch()
+
+    function handleClick(){
+      const index = userArray.findIndex(user => user.username === username)
+      
+      if(index === userArray.length - 2){
+        dispatch(changeUser(userArray[0].username))
+      } else {
+        dispatch(changeUser(userArray[index + 1].username))
+      }
+    }
+
+  return (
+    <StyledUserProfile>
+        <StyledImage onClick={handleClick} src={image} />
+      <Username>{username}</Username>  
+    </StyledUserProfile>
+  )
+}
+
+export default UserProfile
