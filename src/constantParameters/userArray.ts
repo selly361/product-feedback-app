1bf4387c02e6353f028134ef82f7a16e079c78df
seikou <seikou615@gmail.com>
2023-07-30 16:05:16 +0100

    Update: image path

--- src/constantParameters/userArray.ts
+++ src/constantParameters/userArray.ts
@@ -2,62 +2,62 @@ import { User } from "types"
 
 export const userArray: User[] = [
     {
-      image: "user-images/image-zena.jpg",
+      image: "/user-images/image-zena.jpg",
       name: "Zena Kelley",
       username: "velvetround"
     },
     {
-      image: "user-images/image-suzanne.jpg",
+      image: "/user-images/image-suzanne.jpg",
       name: "Suzanne Chang",
       username: "upbeat1811"
     },
     {
-      image: "user-images/image-thomas.jpg",
+      image: "/user-images/image-thomas.jpg",
       name: "Thomas Hood",
       username: "brawnybrave"
     },
     {
-      image: "user-images/image-elijah.jpg",
+      image: "/user-images/image-elijah.jpg",
       name: "Elijah Moss",
       username: "hexagon.bestagon"
     },
     {
-      image: "user-images/image-james.jpg",
+      image: "/user-images/image-james.jpg",
       name: "James Skinner",
       username: "hummingbird1"
     },
     {
-      image: "user-images/image-anne.jpg",
+      image: "/user-images/image-anne.jpg",
       name: "Anne Valentine",
       username: "annev1990"
     },
     {
-      image: "user-images/image-ryan.jpg",
+      image: "/user-images/image-ryan.jpg",
       name: "Ryan Welles",
       username: "voyager.344"
     },
     {
-      image: "user-images/image-george.jpg",
+      image: "/user-images/image-george.jpg",
       name: "George Partridge",
       username: "soccerviewer8"
     },
     {
-      image: "user-images/image-javier.jpg",
+      image: "/user-images/image-javier.jpg",
       name: "Javier Pollard",
       username: "warlikeduke"
     },
     {
-      image: "user-images/image-roxanne.jpg",
+      image: "/user-images/image-roxanne.jpg",
       name: "Roxanne Travis",
       username: "peppersprime32"
     },
     {
-      image: "user-images/image-victoria.jpg",
+      image: "/user-images/image-victoria.jpg",
       name: "Victoria Mejia",
       username: "arlen_the_marlin"
     },
     {
-      image: "user-images/image-jackson.jpg",
+      image: "/user-images/image-jackson.jpg",
       name: "Jackson Barker",
       username: "countryspirit"
     }
