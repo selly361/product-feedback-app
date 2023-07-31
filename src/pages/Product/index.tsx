e22af0f9646daa0ae30e5135a487f1be3b1e38ec
seikou <seikou615@gmail.com>
2023-07-31 12:38:17 +0100

    Refactor: with components and logic

--- src/pages/Product/index.tsx
+++ src/pages/Product/index.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import { Comments, Product, ProductHeader } from 'components'
+
+import AddComment from 'components/PageTemplates/Product/AddComment'
+import { StyledProduct } from "./Product.styles"
+import { useAppSelector } from 'app/hooks'
+import { useParams } from 'react-router-dom'
 
 const ProductPage = () => {
+  
+  const { id } = useParams<{ id: string }>()
+  
+    const { suggestions, roadmap } = useAppSelector(state => state)
+  
+    const productReq = [...suggestions, ...roadmap].find(product => product.id === id)
+  
   return (
-    <div>ProductPage</div>
+    <StyledProduct>
+      <ProductHeader />
+      {productReq ? <Product productReq={productReq} /> : null}
+      <Comments comments={productReq?.comments} />
+      <AddComment />
+    </StyledProduct>
   )
 }
 
 export default ProductPage
+
+
+
+
