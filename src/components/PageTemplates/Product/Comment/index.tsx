6a0387ed050412db57be108be4bbb4e90bb9bca6
seikou <seikou615@gmail.com>
2023-07-31 12:31:56 +0100

    Add: `Comment` component

---
+++ src/components/PageTemplates/Product/Comment/index.tsx
@@ -0,0 +1,42 @@
+import {
+  Container,
+  Message,
+  ReplyButton,
+  StyledComment,
+  StyledImage,
+  StyledName,
+  StyledReplyTo,
+  StyledUsername,
+  SubContainer,
+  TinyContainer
+} from "./Comment.styles";
+
+import AddReply from "../AddReply";
+import { CommentProps } from "./Comment.types";
+import { useState } from "react";
+
+function Comment({ info, type }: CommentProps) {
+  const [reply, setReply] = useState(false);
+
+  return (
+    <StyledComment className={type}>
+      <StyledImage src={info.user.image} alt={info.user.name} />
+      <Container>
+        <TinyContainer>
+          <StyledName>{info.user.name}</StyledName>
+          <StyledUsername>@{info.user.username}</StyledUsername>
+        </TinyContainer>
+        <ReplyButton onClick={() => setReply((e) => !e)}>Reply</ReplyButton>
+        <Message>
+          {type === "reply" ? (
+            <StyledReplyTo>{`@${info?.replyingTo} `}</StyledReplyTo>
+          ) : null}
+          {info.content}
+        </Message>
+        {reply ? <AddReply closeReply={() => setReply(false)} commentId={info.commentId} replyingTo={info.replyingTo} /> : null}
+      </Container>
+    </StyledComment>
+  );
+}
+
+export default Comment;
