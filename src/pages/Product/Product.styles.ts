568b0dcba70d355b2f351478a09c25a06c182a18
seikou <seikou615@gmail.com>
2023-07-31 12:36:38 +0100

    Add: `Product` pages styles

--- src/pages/Product/Product.styles.ts
+++ src/pages/Product/Product.styles.ts
@@ -1,1 +1,11 @@
 import { styled } from "styled-components";
+
+export const StyledProduct = styled.main`
+    height: 1571px;
+    width: 730px;
+    margin-top: 80px;
+
+    display: flex;
+    flex-direction: column;
+    gap: 24px;
+`
