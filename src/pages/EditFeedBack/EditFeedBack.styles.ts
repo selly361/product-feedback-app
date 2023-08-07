16047dc9174d4637413c23cd18b45bdb355d367c
seikou <seikou615@gmail.com>
2023-08-07 13:10:03 +0100

    Update: added `EditFeedBack`  components styles

--- src/pages/EditFeedBack/EditFeedBack.styles.ts
+++ src/pages/EditFeedBack/EditFeedBack.styles.ts
@@ -1,1 +1,13 @@
 import { styled } from "styled-components";
+
+export const StyledEditFeedBack = styled.main`
+    min-height: 730px;
+    width: 540px;
+    padding-top: 92px;
+    margin-bottom: 129px;
+
+    display: flex;
+    flex-direction: column;
+    gap: 68px;
+`
+
