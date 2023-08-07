a2c27d5b96f09b659a52b72ff7ae42893cfb13b2
seikou <seikou615@gmail.com>
2023-08-07 12:59:06 +0100

    Add: `AddFeedBackForm` components styles

---
+++ src/components/PageTemplates/AddFeedBack/AddFeedBackForm/AddFeedBackForm.styles.ts
@@ -0,0 +1,23 @@
+import { NewFeedbackIcon } from "icons";
+import styled from "styled-components";
+
+export const AddIcon = styled(NewFeedbackIcon)`
+    position: absolute;
+    top: -23px;
+    left: 42px;
+
+`
+
+
+export const Buttons = styled.div`
+  max-width: 100%;
+  display: flex;
+  justify-content: end;
+  gap: 16px;
+  align-items: center;
+
+
+  button:first-child {
+    width: 93px;
+  }
+`
