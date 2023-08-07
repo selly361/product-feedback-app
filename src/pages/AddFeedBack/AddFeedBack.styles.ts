a9b566de17e5d09bbe1be1831ad69dc2badcbfdd
seikou <seikou615@gmail.com>
2023-08-07 13:09:49 +0100

    Update: added `AddFeedBack`  components styles

--- src/pages/AddFeedBack/AddFeedBack.styles.ts
+++ src/pages/AddFeedBack/AddFeedBack.styles.ts
@@ -1,1 +1,13 @@
 import { styled } from "styled-components";
+
+export const StyledAddFeedBack = styled.main`
+    height: 730px;
+    width: 540px;
+    padding-top: 92px;
+    margin-bottom: 129px;
+
+    display: flex;
+    flex-direction: column;
+    gap: 68px;
+`
+
